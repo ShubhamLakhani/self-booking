fc5758aa72033e23c0c9c06f931b45b03d6dbc67
ShubhamLakhani <lakhanishubham9750@gmail.com>
2025-05-07 19:06:26 +0530

    fix

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Widget from '~/components/widget/Widget';
 
 export default function EmbedPage() {
