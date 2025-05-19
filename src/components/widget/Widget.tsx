aaf0b2967d8caffdd005751eab8bfa31ecba0932
ShubhamLakhani <89403694+ShubhamLakhani@users.noreply.github.com>
2025-05-19 19:48:10 +0530

    Update Widget.tsx

--- src/components/widget/Widget.tsx
+++ src/components/widget/Widget.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import FloatingButton from './FloatingButton';
 import WelcomeScreen from './WelcomeScreen';
 import BookingForm from './BookingForm';
